878137cff9247b27e2fac84184e9a1d1d51654be
angsar210600 <47266436+angsar210600@users.noreply.github.com>
2019-02-10 16:18:48 +0600

    Week1

---
+++ Week1/Task1/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+            static bool isprime(int n)//создал метод для проверки чисел на праймство
+            {
+                if (n < 2) return false;
+                for (int i = 2; i * i <= n; i++)
+                {
+                    if (n % i == 0) { return false; }
+                }
+                return true;
+            }
+            static void Main(string[] args)
+            {
+                int n = int.Parse(Console.ReadLine());//считивал число через консол
+                int[] a = new int[n];//создал массив
+                string[] s = new string[n];//создал массив строков
+                s = Console.ReadLine().Split();//считываю массив и через сплит мы передаем массив чаров
+                int count = 0;//переменная для считание количесво праймов
+                for (int i = 0; i < n; i++)
+                {
+                    a[i] = int.Parse(s[i]);//считываем в массив
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    if (isprime(a[i]))
+                    {
+                        count++;//считаем количесво праймов
+                    }
+                }
+                Console.WriteLine(count);//и выводим
+                for (int i = 0; i < n; i++)
+                {
+                    if (isprime(a[i]))
+                    {
+                        Console.Write(a[i] + " ");
+                    }//и показываем на консоле прайм намберов из массива
+                }
+                Console.ReadKey();
+            }
+      
+    }
+}
---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());// считываем переменную из консол
+            int[] a = new int[n];
+            string[] s = new string[n];
+            s = Console.ReadLine().Split();//cчитаваем с помощи сплит элеменов массива
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(s[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + " ");//делаю ввыд два раза
+                Console.Write(a[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
